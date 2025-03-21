074193efdf5c87b50819d59fc860496a42d7bf12
Barrett <barrettkowalsky@gmail.com>
2025-03-21 07:48:51 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import HomeInputs from './components/HomeInputs'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1> Dimo Login Generator </h1>
+      </div>
+
+      <HomeInputs />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HomeInputs.tsx
@@ -0,0 +1,12 @@
+
+function HomeInputs(){
+    return (
+        <form>
+            <input type="text" placeholder="Enter a Client ID" />
+            <input type="text" placeholder="Enter a Redirect URI" />
+            <button> Create My Link</button>
+        </form>
+    )
+}
+
+export default HomeInputs
