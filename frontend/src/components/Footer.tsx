cf0751acee9d8e1d0b02cd9e6721b9d825f69499
Barrett <barrettkowalsky@gmail.com>
2025-03-24 08:46:36 -0600

    components

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <div>
-            <p>© 2021 Dimo Network</p>
+            <p>© 2025 Dimo Network</p>
             <p><a href="https://docs.dimo.org/developer-platform">DIMO Developer Docs</a></p>
             <p>Contact Support: <a href="mailto:developer@dimo.org"> developer@dimo.org</a></p>
         </div>
--- frontend/src/components/LoginConfirmation.tsx
+++ frontend/src/components/LoginConfirmation.tsx
@@ -4,7 +4,6 @@ import {
   } from "@dimo-network/login-with-dimo"; 
 import { useLocation } from "react-router-dom";
 
-
 export default function LoginConfirmation(){
     const location = useLocation();
     const {
@@ -14,11 +13,13 @@ export default function LoginConfirmation(){
         userExpirationDate
     } = location.state || {};
 
+
     initializeDimoSDK({
         clientId: userClientId,
         redirectUri: userRedirectUri,
         environment: "production",
       });
+
 
     return(
         <DimoAuthProvider>
@@ -33,11 +34,12 @@ export default function LoginConfirmation(){
                     <LoginWithDimo
                         mode="redirect" />
                 </div>
+                
                 <h3>Your custom login URL:</h3>
                 <p>https://login.dimo.org/?clientId={userClientId}&redirectUri={userRedirectUri}&permissionTemplateId={userPermissionTemplateId}&entryState=VEHICLE_MANAGER&expirationDate={userExpirationDate} </p>
                 <h3>Using the DIMO Login React Component SDK: </h3>
                 <p>To use the Login with DIMO React Component SDK, first install:</p>
-                <p>npm install npm i @dimo-network/login-with-dimo</p>
+                <p>npm i @dimo-network/login-with-dimo</p>
                 <p>Then, import the components:</p>
                 <pre>
                     {`import { initializeDimoSDK } from "@dimo-network/login-with-dimo";
